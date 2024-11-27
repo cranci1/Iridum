7afc7ea7a8a35e3d3be14b3c988bddd81be8be03
Francesco <100066266+cranci1@users.noreply.github.com>
2024-11-27 14:50:15 +0100

    adjusted structure

--- Iridum/MediaDetailView.swift
+++ Iridum/MediaDetailView.swift
@@ -5,10 +5,10 @@
 //  Created by Francesco on 25/11/24.
 //
 
+import AVKit
 import SwiftUI
 import Kingfisher
 import SwiftSoup
-import AVKit
 
 struct Episode: Identifiable {
     let id: Int
@@ -413,53 +413,5 @@ struct MediaDetailView: View {
         } catch {
             print("Failed to set up AVAudioSession: \(error)")
         }
-    }
-}
-
-class NormalPlayer: AVPlayerViewController {
-    private var originalRate: Float = 1.0
-    private var holdGesture: UILongPressGestureRecognizer?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupHoldGesture()
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UserDefaults.standard.bool(forKey: "AlwaysLandscape") {
-            return .landscape
-        } else {
-            return .all
-        }
-    }
-    
-    private func setupHoldGesture() {
-        holdGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleHoldGesture(_:)))
-        holdGesture?.minimumPressDuration = 0.5
-        if let holdGesture = holdGesture {
-            view.addGestureRecognizer(holdGesture)
-        }
-    }
-    
-    @objc private func handleHoldGesture(_ gesture: UILongPressGestureRecognizer) {
-        switch gesture.state {
-        case .began:
-            beginHoldSpeed()
-        case .ended, .cancelled:
-            endHoldSpeed()
-        default:
-            break
-        }
-    }
-    
-    private func beginHoldSpeed() {
-        guard let player = player else { return }
-        originalRate = player.rate
-        let holdSpeed = UserDefaults.standard.float(forKey: "holdSpeedPlayer")
-        player.rate = holdSpeed
-    }
-    
-    private func endHoldSpeed() {
-        player?.rate = originalRate
     }
 }
